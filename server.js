4e5e6703949f18624b44675f4ffa837f109a4395
amritakc <amritakc@gmail.com>
2016-03-29 19:04:42 -0700

    testing .gitignore

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser'); 
+var Twit = required('twit');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
