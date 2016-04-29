11b022871312da98cd76a1a9c1e1bc9c84450245
amritakc <amritakc@gmail.com>
2016-04-29 13:30:26 -0700

    completed login feature

--- server/config/routes.js
+++ server/config/routes.js
@@ -12,7 +12,7 @@ module.exports = function(app){
 	app.post('/register', function(req, res, next){
 		users.create(req,res, next)
 	})
-	app.post('/login ', function(req, res, next){
+	app.post('/login', function(req, res, next){
 		users.show(req,res, next)
 	})
 };
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -36,7 +36,37 @@ module.exports = (function() {
 				console.log("successfully added a user")
 				return res.json({token: user.generateJWT()})
 			});
-		}
+		},
+		show: function(req, res, next){
+			console.log("in show method")
+			console.log(req.body)
+			if(!req.body.username || !req.body.password){
+				console.log("both fields not filled out")
+				var message = "Please complete both fields"
+				return res.json(message);
+			}
+
+			passport.authenticate('local', function(err, user, info){
+				console.log("in passport authenticate")
+				if(err){ 
+					console.log(err)
+					console.log("in err for passport authenticate")
+					return next(err); 
+				}
+
+				if(user){
+					console.log("found user")
+					return res.json({token: user.generateJWT()});
+				} 
+				else {
+					console.log("hello from else")
+					console.log(info)
+					var message = "Incorrect username/password"
+					return res.json(message);
+				}
+			})(req, res, next);
+		},
+
 
 
  
