b33289a741d0dc2c4f2010f009c32c0f299a8188
amritakc <amritakc@gmail.com>
2016-04-28 17:43:28 -0700

    completed registration mfeature

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,8 +1,44 @@
 var mongoose = require('mongoose');
 var User = mongoose.model('User');
+var passport = require('passport');
 
 module.exports = (function() {
  return {
+
+
+		create: function(req, res, next){
+			console.log(req.body)
+			if(!req.body.username || !req.body.password || !req.body.email){
+				var message = "Please complete all fields"
+				return res.json(message)
+			}
+
+			if(req.body.password !== req.body.password2){
+				var message = "Passwords do not match"
+				return res.json(message)
+			}
+
+			var user = new User();
+
+			user.username = req.body.username;
+
+			user.email = req.body.email;
+
+			user.setPassword(req.body.password)
+
+			user.save(function(err){
+				//console.log(user.username)
+				if(err){ 
+					console.log("something went wrong"); 
+					var message = "User already exists"
+					return res.json(message)
+				}
+				console.log("successfully added a user")
+				return res.json({token: user.generateJWT()})
+			});
+		}
+
+
  
 	}	
 })(); 
--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
 var secret = "sauce"
+var jwt = require('jsonwebtoken');
 
 var UserSchema = new mongoose.Schema({
   username: {type: String, unique: true},
@@ -19,6 +20,19 @@ UserSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
   return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 1);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, secret);
 };
 
 mongoose.model('User', UserSchema);
